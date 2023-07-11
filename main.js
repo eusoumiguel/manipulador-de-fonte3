e2f391a20c7c9abe44b07731f403c519c1a73019
eusoumiguel <113737239+eusoumiguel@users.noreply.github.com>
2023-07-11 11:24:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,64 +1,42 @@
-noseX=0;
-noseY=0;
 difference = 0;
 rightWristX = 0;
 leftWristX = 0;
 
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
 
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
 
-
-function setup() {
-    video = createCapture(VIDEO);
-    video.size(550, 500);
-    
-    canvas = createCanvas(500, 500);
-    canvas.position(560, 150);
-    
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-function preload() {
-    classifier = ml5.imageClassifier('DoodleNet');
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
 }
 
-function clearCanvas(){
-    background("white");
-}
 
-function draw() {
-    strokeWeight(10);
-    stroke(0);
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
 
-    if (mouseIsPressed) {
-        line(pmouseX, pmouseY, mouseX, mouseY);
-    }
-}
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
 
-function classifyCanvas() {
-    classifier.classify(canvas, gotResult);
-}
-
-function gotResult(results) {
-    if (results.length > 0) {
-        console.log(results);
-        noseX = results[0].pose.nose.x;
-        noseY = results[0].pose.nose.y;
-        console.log("noseX = " + noseX + "noseY = " + noseX);
-
-        leftWristX = results[0].pose.leftWrist.x;
-        rightWristX = results[0].pose.rightWristX.x;
-        difference = floor(leftWristX - rightWrist);
-
-        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference =" + difference);
-    }
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
 }
 
 function draw() {
-    background('#969A97');  
+background('#6C91C2');
 
-    document.getElementById("square_side").innerHTML = "Largura e altura serão = " + difference + "px";
-    fill('#F90093');
-    stroke('#F90093');
-    square(noseX, noseY, difference);
+  document.getElementById("font_size").innerHTML = "Tamanho da fonte será = " + difference +"px";
+textSize(difference);
+fill('#FFE787');
+text('Pedro', 50, 400);
 }
